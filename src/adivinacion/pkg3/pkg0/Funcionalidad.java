2d4b339f923964a6c0cda04fa6dde55310310f6e
yoyo <yoyo@yoyo-vaio>
2020-03-13 14:33:40 -0600

    Adivinacion 3.0 terminado en consola

--- src/adivinacion/pkg3/pkg0/Funcionalidad.java
+++ src/adivinacion/pkg3/pkg0/Funcionalidad.java
@@ -7,10 +7,7 @@ package adivinacion.pkg3.pkg0;
 
 import java.util.Scanner;
 import adivinacion.pkg3.pkg0.libreria.dominio;
-import static java.lang.Thread.sleep;
 import java.util.Collections;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -34,7 +31,26 @@ public class Funcionalidad {
             System.out.println("\nya barajado: ");
             baraja();
             imprime();
-        }else{
+            System.out.println("\n\nPiense en una carta y que se le quede... \n");
+            System.out.println("la cantidad de tiradas"+calculo(33, 3));
+            juego(calculo(cant, 3));
+            
+            System.out.println("Desea volver a jugar? s/n");
+            String elec=instruccion.next();
+            switch(elec){
+                case "s":
+                    iniciar();
+                    break;
+                case "n":
+                    System.out.println("adios...");
+                    break;
+                default:
+                    System.out.println("Elija una opcion valida...");
+                    iniciar();
+                    break;
+            }
+
+        } else {
             System.out.println("El numero de cartas elegido debe ser multiplo de 3 impar... (no mayor a 100)");
             iniciar();
         }
@@ -55,11 +71,127 @@ public class Funcionalidad {
 
     private void imprime() {
         for (int i = 0; i < cant; i++) {
-            System.out.print(Carta.cartas.get(i).valor+ "... ");
+            System.out.print(Carta.cartas.get(i).valor + "... ");
+        }
+    }
+
+    private void baraja() {
+        Collections.shuffle(Carta.cartas);
+    }
+
+    private void juego(int noTiros) {
+        int contador = 0;
+        while (noTiros != contador) {
+            tirada();
+            grupoElegido();
+            System.out.println("La nueva baraja queda asi: ");
+            imprime();
+            System.out.println("\n");
+            contador++;
+        }
+        resultado();
+    }
+
+    private void tirada() {
+        Carta.grupo1.clear();
+        Carta.grupo2.clear();
+        Carta.grupo3.clear();
+        int contador = 0;
+        for (int i = 0; i < Carta.cartas.size() / 3; i++) {
+            Carta.grupo1.add(Carta.cartas.get(contador));
+            Carta.grupo2.add(Carta.cartas.get(contador + 1));
+            Carta.grupo3.add(Carta.cartas.get(contador + 2));
+            contador++;
+            contador++;
+            contador++;
+        }
+        System.out.println("Grupo 1: ");
+        imprimeGrupo1();
+        System.out.println("Grupo 2: ");
+        imprimeGrupo2();
+        System.out.println("Grupo 3: ");
+        imprimeGrupo3();
+    }
+
+    private void reordenamiento(int no) {
+        switch (no) {
+            case 1:
+                Carta.cartas.clear();
+                for (int i = 0; i < Carta.grupo2.size(); i++) {
+                    Carta.cartas.add(Carta.grupo2.get(i));
+                }
+                for (int i = 0; i < Carta.grupo1.size(); i++) {
+                    Carta.cartas.add(Carta.grupo1.get(i));
+                }
+                for (int i = 0; i < Carta.grupo3.size(); i++) {
+                    Carta.cartas.add(Carta.grupo3.get(i));
+                }
+                break;
+            case 2:
+                Carta.cartas.clear();
+                for (int i = 0; i < Carta.grupo1.size(); i++) {
+                    Carta.cartas.add(Carta.grupo1.get(i));
+                }
+                for (int i = 0; i < Carta.grupo2.size(); i++) {
+                    Carta.cartas.add(Carta.grupo2.get(i));
+                }
+                for (int i = 0; i < Carta.grupo3.size(); i++) {
+                    Carta.cartas.add(Carta.grupo3.get(i));
+                }
+                break;
+            case 3:
+                Carta.cartas.clear();
+                for (int i = 0; i < Carta.grupo1.size(); i++) {
+                    Carta.cartas.add(Carta.grupo1.get(i));
+                }
+                for (int i = 0; i < Carta.grupo3.size(); i++) {
+                    Carta.cartas.add(Carta.grupo3.get(i));
+                }
+                for (int i = 0; i < Carta.grupo2.size(); i++) {
+                    Carta.cartas.add(Carta.grupo2.get(i));
+                }
+                break;
+            default:
+                System.out.println("sucedio un error inesperado, aiuuuuuda D:");
         }
     }
     
-    private void baraja(){
-     Collections.shuffle(Carta.cartas);
+    private void resultado(){
+        int posicion= ((cant+1)/2)-1;
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("El numero que usted penso es: "+ Carta.cartas.get(posicion).valor);
+        System.out.println("----------------------------------------------------------------------");
+    }
+
+    private void grupoElegido() {
+        System.out.println("En que Grupo se encuentra la carta que penso?");
+        int elecc = instruccion.nextInt();
+        if ((elecc == 3) || (elecc == 2) || (elecc == 1)) {
+            reordenamiento(elecc);
+        } else {
+            System.out.println("Porfavor elija un grupo valido");
+            grupoElegido();
+        }
+    }
+
+    private void imprimeGrupo1() {
+        for (int i = 0; i < Carta.grupo1.size(); i++) {
+            System.out.println(Carta.grupo1.get(i).valor + "... ");
+        }
+        System.out.println("\n");
+    }
+
+    private void imprimeGrupo2() {
+        for (int i = 0; i < Carta.grupo2.size(); i++) {
+            System.out.println(Carta.grupo2.get(i).valor + "... ");
+        }
+        System.out.println("\n");
+    }
+
+    private void imprimeGrupo3() {
+        for (int i = 0; i < Carta.grupo3.size(); i++) {
+            System.out.println(Carta.grupo3.get(i).valor + "... ");
+        }
+        System.out.println("\n");
     }
 }
